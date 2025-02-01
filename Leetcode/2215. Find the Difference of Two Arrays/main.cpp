4c3c0e7bbe7349c691a66fa921bf2cd7e1e6063b
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-01 18:28:26 +0300

    2215. Find the Difference of Two Arrays - Accepted

---
+++ Leetcode/2215. Find the Difference of Two Arrays/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> answer(2);
+        unordered_set<int> set1;
+        unordered_set<int> set2;
+        
+        for(int i : nums1){
+            set1.insert(i);
+        }
+        for(int i : nums2){
+            set2.insert(i);
+        }
+        
+        for(int num : set1){
+            if(set2.find(num) == set2.end()){
+                answer[0].push_back(num);
+            }
+        }
+        
+        for(int num : set2){
+            if(set1.find(num) == set1.end()){
+                answer[1].push_back(num);
+            }
+        }
+        return answer;
+    }
+    
+};
+int main() {
+    Solution s;
+    vector<int> nums1 = {1,2,3};
+    vector<int> nums2 = {2,4,6};
+
+    s.findDifference(nums1, nums2);
+    return 0;
+}
