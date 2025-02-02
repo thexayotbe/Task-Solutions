00fccfd3928ae8e2008ae1408d0377c4964faa95
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-02 12:51:36 +0300

    1752. Check if Array Is Sorted and Rotated - Solved

--- Leetcode/1752. Check if Array Is Sorted and Rotated/main.cpp
+++ Leetcode/1752. Check if Array Is Sorted and Rotated/main.cpp
@@ -1,11 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>  // For std::find
+
+using namespace std;
 class Solution {
 public:
     bool check(vector<int>& nums) {
         int count = 0;
         const int n = nums.size();
-        for(int i =0; i < n && count <=1; i++){
+        for(int i =1; i < n && count <=1; i++){
             if(nums[i] < nums[i-1]) count ++;
         }
         return (nums[0] >= nums.back() && count == 1) || count == 0;
     }
 };
+int main() {
+    Solution s;
+    vector<int> nums1 = {1,2,3};
+    cout << s.check(nums1) << endl;
+
+   
+    return 0;
+}
