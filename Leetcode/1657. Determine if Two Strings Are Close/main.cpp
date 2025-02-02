66b262ee6db6a25770f529f9e58d2accbe6f24f2
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-02 13:06:42 +0300

    1657. Determine if Two Strings Are Close

---
+++ Leetcode/1657. Determine if Two Strings Are Close/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>  // For std::find
+using namespace std;
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        return true;
+    }
+};
+int main() {
+    Solution s;
+    string  word1 = "abc", word2 = "bca";
+    cout << s.closeStrings(word1, word2) << endl;
+
+   
+    return 0;
+}
