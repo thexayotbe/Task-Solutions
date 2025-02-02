d4c94227cc8993831bb983d4e24656d25e2196c6
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-02 13:14:42 +0300

    1657. Determine if Two Strings Are Close Solution -v1

--- Leetcode/1657. Determine if Two Strings Are Close/main.cpp
+++ Leetcode/1657. Determine if Two Strings Are Close/main.cpp
@@ -6,6 +6,31 @@ using namespace std;
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        vector<int> w1Freq(26,0);
+        vector<int>w2Freq(26,0);
+        
+        for(int ch : word1) {
+            w1Freq[ch - 'a']++;
+        }
+        
+        for(int ch : word2) {
+            w2Freq[ch - 'a']++;
+        }
+        
+        for(int i = 0; i< 26 ; i++){
+            if((w1Freq[i] == 0 && w2Freq[i]!=0) || (w2Freq[i] == 0 && w1Freq[i]!=0)){
+                return false;
+            }
+        }
+        
+        sort(w1Freq.begin(), w1Freq.end());
+        sort(w2Freq.begin(), w2Freq.end());
+
+        for(int i = 0; i < 26; i++){
+            if(w1Freq[i]!= w2Freq[i]) {
+                return false;
+            }
+        }
         return true;
     }
 };
