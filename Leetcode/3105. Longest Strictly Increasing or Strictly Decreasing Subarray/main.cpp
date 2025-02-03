d57424e433073fd4b10067f21daa095174c0e53e
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-03 15:42:05 +0300

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray - Passed V1

---
+++ Leetcode/3105. Longest Strictly Increasing or Strictly Decreasing Subarray/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int inc = 1;
+        int dec = 1;
+        int n = nums.size();
+        int ans = 0;
+        if(n == 1) return 1;
+        
+    
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[i-1]) {
+                inc++;
+                dec  = 1;
+            }
+            else if(nums[i] < nums[i-1]){
+                dec++;
+                inc = 1;
+            }
+            else {
+                dec=inc= 1;
+            }
+            ans = max({ans, dec, inc});
+        }
+        return ans;
+        
+    }
+};
+
+
+
+int main() {
+    Solution s;
+    vector<int> nums = {3,2,1};
+    cout << s.longestMonotonicSubarray(nums) << endl;
+    return 0;
+}
