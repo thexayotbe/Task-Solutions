1a61ae20d0a572f3c17d4e38842570fbfb6ff716
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-03 15:43:03 +0300

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray - Formated

--- Leetcode/3105. Longest Strictly Increasing or Strictly Decreasing Subarray/main.cpp
+++ Leetcode/3105. Longest Strictly Increasing or Strictly Decreasing Subarray/main.cpp
@@ -5,24 +5,21 @@ public:
         int dec = 1;
         int n = nums.size();
         int ans = 0;
-        if(n == 1) return 1;
-        
-    
-        for(int i = 1; i < n; i++){
-            if(nums[i] > nums[i-1]) {
+        if (n == 1)
+            return 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
                 inc++;
-                dec  = 1;
-            }
-            else if(nums[i] < nums[i-1]){
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
                 dec++;
                 inc = 1;
-            }
-            else {
-                dec=inc= 1;
+            } else {
+                dec = inc = 1;
             }
             ans = max({ans, dec, inc});
         }
         return ans;
-        
     }
 };
