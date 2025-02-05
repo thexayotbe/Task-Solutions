e19d6593d612d4ac91b1817b4564bb9a82975152
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-05 09:42:14 +0300

    1790. Check if One String Swap Can Make Strings Equal - Cleaned

--- Leetcode/1790. Check if One String Swap Can Make Strings Equal/main.cpp
+++ Leetcode/1790. Check if One String Swap Can Make Strings Equal/main.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         int n = s1.size();
-        int  x = -1;
-        for(int i = 0; i < n; i++){
-            if(s1[i] !=(s2[i])){
-                if(x==-1) x = i;
+        int x = -1;
+        for (int i = 0; i < n; i++) {
+            if (s1[i] != (s2[i])) {
+                if (x == -1)
+                    x = i;
                 else {
                     swap(s1[i], s1[x]);
                     return s1 == s2;
@@ -14,5 +15,4 @@ public:
         }
         return s1 == s2;
     }
-    
 };
