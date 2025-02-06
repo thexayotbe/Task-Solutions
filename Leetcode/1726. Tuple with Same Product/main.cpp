82deeb5cf60cc6e71006d439ad72e8ceb7010660
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-06 12:09:48 +0300

    1726. Tuple with Same Product - Beats 83% in Runtime 299ms

--- Leetcode/1726. Tuple with Same Product/main.cpp
+++ Leetcode/1726. Tuple with Same Product/main.cpp
@@ -4,20 +4,20 @@ public:
         int res = 0;
         int n = nums.size();
         unordered_map<int, int> permutations;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1 ; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int mult = nums[i] * nums[j];
                 permutations[mult]++;
             }
         }
-        for(auto pair : permutations){
-            int count = pair.second;
-            if(count > 1){
-                res += count * (count-1) * 4;
 
+        for (auto pair : permutations) {
+            int count = pair.second;
+            if (count > 1) {
+                res += count * (count - 1) * 4;
             }
         }
-        
+
         return res;
     }
 };
