909cf832c792eb0153761505b28158563e614626
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-06 11:46:16 +0300

    1726. Tuple with Same Product - Solved

--- Leetcode/1726. Tuple with Same Product/main.cpp
+++ Leetcode/1726. Tuple with Same Product/main.cpp
@@ -3,4 +3,40 @@ public:
     int tupleSameProduct(vector<int>& nums) {
         
     }
+};// Mamajonov Khayotbek DSAI - 02
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        unordered_map<int, int> permutations;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1 ; j < n; j++){
+                int mult = nums[i] * nums[j];
+                permutations[mult]++;
+            }
+        }
+        
+        
+        for(auto pair : permutations){
+            int count = pair.second;
+            if(count > 1){
+                res += count * (count-1) * 4;
+
+            }
+        }
+        
+        return res;
+    }
 };
+int main() {
+    Solution s;
+    vector<int> nums = {2,3,4,6,8,12};
+    cout << s.tupleSameProduct(nums) << endl;
+    return 0;
+}
