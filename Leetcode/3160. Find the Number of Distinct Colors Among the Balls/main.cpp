b116ab91ade83423ed30a73b1cc7e0831f8e89fc
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-07 14:15:13 +0300

    3160. Find the Number of Distinct Colors Among the Balls - Solved

--- Leetcode/3160. Find the Number of Distinct Colors Among the Balls/main.cpp
+++ Leetcode/3160. Find the Number of Distinct Colors Among the Balls/main.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map<int, int> node, color;
+        unordered_map<int,int> ball, color;
         vector<int> ans;
-
-        for (auto& q : queries) {
-            int x = q[0], y = q[1];
-
-            if (node.count(x)) {
-                int prevColor = node[x];
-                if (prevColor == y) {
-                    ans.push_back(color.size());
-                    continue;
-                }
-                if (--color[prevColor] == 0) color.erase(prevColor);
-            }
-
-            node[x] = y;
-            color[y]++;
-            ans.push_back(color.size());
+        ans.reserve(queries.size());
+        int distinct = 0;
+        for(auto &q : queries) {
+            int pos = q[0], c = q[1];
+            if(ball.count(pos))
+                if(--color[ball[pos]] == 0) distinct--;
+            ball[pos] = c;
+            if(++color[c] == 1) distinct++;
+            ans.push_back(distinct);
         }
         return ans;
     }
