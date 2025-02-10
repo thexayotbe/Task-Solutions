d083fd567d23dae47b97afcc86010f90b1b23ee5
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-10 10:02:28 +0300

    3174. Clear Digits - Started - Passed Test CAse

---
+++ Leetcode/3174. Clear Digits/main.cpp
@@ -0,0 +1,39 @@
+// Mamajonov Khayotbek DSAI - 02
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res = "";
+       
+        int count = 0;
+
+        for(int i = s.size()-1; i >= 0; i--){
+            if(isDig(s[i])){
+                s.erase(i,1);
+                count++;
+                continue;
+            }
+            if(count > 0){
+                s.erase(i,1);
+                count--;
+            }
+            
+        }
+
+        return s;
+    }
+    
+    private: bool isDig(char n){
+        return n == '1' || n == '2' || n == '3' || n == '4' || n== '5' || n== '6' || n == '7' || n== '8' || n == '9' || n == '0';
+    }
+};
+int main() {
+    Solution s;
+    string string = "cb3";
+    cout << s.clearDigits(string) << endl;
+    return 0;
+}
