10c0f0cb5b9e16ca6ebd4b3bae82127de440a2c3
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-10 10:03:15 +0300

    3174. Clear Digits - Started - Passed Test CAse

--- Leetcode/3174. Clear Digits/main.cpp
+++ Leetcode/3174. Clear Digits/main.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     string clearDigits(string s) {
         string res = "";
-       
         int count = 0;
-
         for(int i = s.size()-1; i >= 0; i--){
             if(isDig(s[i])){
                 s.erase(i,1);
@@ -17,10 +15,8 @@ public:
             }
             
         }
-
         return s;
     }
-    
     private: bool isDig(char n){
         return n == '1' || n == '2' || n == '3' || n == '4' || n== '5' || n== '6' || n == '7' || n== '8' || n == '9' || n == '0';
     }
