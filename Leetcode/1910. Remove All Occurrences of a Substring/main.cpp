7861342cfcc319a13b18f7dae39d721ca547d6fa
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-11 14:44:14 +0300

    1910. Remove All Occurrences of a Substring - Beats 100% in Runtime

--- Leetcode/1910. Remove All Occurrences of a Substring/main.cpp
+++ Leetcode/1910. Remove All Occurrences of a Substring/main.cpp
@@ -1,14 +1,12 @@
 class Solution {
-public:
-    string removeOccurrences(string s, string part) {
-        int index = s.find(part);
-        cout << index << endl;
-        while(index != -1){
-            s.erase(index, part.size());
-            index = s.find(part);
+    public:
+        string removeOccurrences(string s, string part) {
+            int index = s.find(part);
+            cout << index << endl;
+            while (index != -1) {
+                s.erase(index, part.size());
+                index = s.find(part);
+            }
+            return s;
         }
-        return s;
-    }
 };
-
-
