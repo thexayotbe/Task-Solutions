8a891339b63e65524fd20ff39400edb6cc026328
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-11 14:30:26 +0300

    1910. Remove All Occurrences of a Substring - Started

---
+++ Leetcode/1910. Remove All Occurrences of a Substring/main.cpp
@@ -0,0 +1,22 @@
+// Khayotbek Mamajonov
+// DSAI - 02
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "daabcbaabcbc", part = "abc";
+    cout << sol.removeOccurrences(s, part);
+    return 0;
+}
