f1ab0c9366207a830e12ff88b5576d314da4817d
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-12 20:28:09 +0300

    2342. Max Sum of a Pair With Equal Sum of Digits Submited

--- Leetcode/2342. Max Sum of a Pair With Equal Sum of Digits/main.cpp
+++ Leetcode/2342. Max Sum of a Pair With Equal Sum of Digits/main.cpp
@@ -1,26 +1,27 @@
+
 class Solution {
-public:
-    int maximumSum(vector<int>& nums) {
-        unordered_map<int, int> digitsSum;
-        int maxNumber = -1;
-        for(int i : nums) {
-            int sumDig = sumOfDig(i);
-            if(digitsSum.count(sumDig)){
-                maxNumber = max(maxNumber, i + digitsSum[sumDig]);
+    public:
+        int maximumSum(vector<int>& nums) {
+            unordered_map<int, int> digitsSum;
+            int maxNumber = -1;
+            for (int i : nums) {
+                int sumDig = sumOfDig(i);
+                if (digitsSum.count(sumDig)) {
+                    maxNumber = max(maxNumber, i + digitsSum[sumDig]);
+                }
+                digitsSum[sumDig] = max(i, digitsSum[sumDig]);
             }
-            digitsSum[sumDig] = max(i,digitsSum[sumDig]);
+            return maxNumber;
         }
-        return maxNumber;
-    }
     
-    
-private:
-    int sumOfDig(int n) {
-        int sum = 0;
-        while (n > 0) {
-            sum += n % 10;  
-            n /= 10;        
+    private:
+        int sumOfDig(int n) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10; // Add the last digit of n to the sum
+                n /= 10;       // Remove the last digit from n
+            }
+            return sum;
         }
-        return sum;
-    }
-};
+    };
+    
