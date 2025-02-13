649322db975a49a03a9909fa3135cf191f38f852
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-13 15:36:50 +0300

    66. Plus One Started

--- Leetcode/66. Plus One/main.cpp
+++ Leetcode/66. Plus One/main.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        
         for (int i = n - 1; i >= 0; --i) {
             if (digits[i] < 9) {
                 digits[i]++;
@@ -10,7 +9,6 @@ public:
             }
             digits[i] = 0;
         }
-        
         digits.insert(digits.begin(), 1);
         return digits;
     }
