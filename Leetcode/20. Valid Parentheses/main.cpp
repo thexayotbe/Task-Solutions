2c2d71e2cc436d997e6b9dfcf66d9aa37e34141e
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-13 15:09:23 +0300

    20. Valid Parentheses  FOrmated

--- Leetcode/20. Valid Parentheses/main.cpp
+++ Leetcode/20. Valid Parentheses/main.cpp
@@ -1,22 +1,19 @@
 class Solution {
-public:
-    bool isValid(string s) {
-        unordered_map<char, char> braces = {{')' , '('},{']', '['},{'}', '{'}};
-        stack<char> st;
-        for(char ch : s){
-            if(braces.count(ch)){
-                if(!st.empty() && st.top() == braces[ch]){
-                    st.pop();
-                }
-                else {
-                    return false;
+    public:
+        bool isValid(string s) {
+            unordered_map<char, char> braces = {{')', '('}, {']', '['}, {'}', '{'}};
+            stack<char> st;
+            for (char ch : s) {
+                if (braces.count(ch)) {
+                    if (!st.empty() && st.top() == braces[ch]) {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    st.push(ch);
                 }
             }
-            else {
-                st.push(ch);
-            }
-            
-        }
-        return st.empty();
-    };
+            return st.empty();
+        };
 };
