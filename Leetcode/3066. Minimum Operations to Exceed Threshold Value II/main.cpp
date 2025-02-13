7ee25288d5f63642b82808a87a5da541b7c45229
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-13 12:54:20 +0300

    3066. Minimum Operations to Exceed Threshold Value II - Started

--- Leetcode/3066. Minimum Operations to Exceed Threshold Value II/main.cpp
+++ Leetcode/3066. Minimum Operations to Exceed Threshold Value II/main.cpp
@@ -1,17 +1,19 @@
 class Solution {
-public:
-    int minOperations(vector<int>& nums, int k) {
-        int count = 0;
-        priority_queue<long long, vector<long long>, greater<>> pq(nums.begin(),
-                                                                   nums.end());
-
-        while (pq.top() < k) {
-            long long x = pq.top(); pq.pop();
-            long long y = pq.top(); pq.pop();
-            pq.push(2*x+y);
-            count++;
-            
+    public:
+        int minOperations(vector<int>& nums, int k) {
+            int count = 0;
+            priority_queue<long long, vector<long long>, greater<>> pq(nums.begin(),
+                                                                       nums.end());
+    
+            while (pq.top() < k) {
+                long long x = pq.top();
+                pq.pop();
+                long long y = pq.top();
+                pq.pop();
+                int r = min(x, y) * 2 + max(x, y);
+                pq.push(2 * x + y);
+                count++;
+            }
+            return count;
         }
-        return count;
-    }
-};
+    };
