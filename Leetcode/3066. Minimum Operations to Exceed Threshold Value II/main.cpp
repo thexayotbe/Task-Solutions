cd2490600218b4231821c9aa43ef66c6ace66e5d
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-13 11:06:31 +0300

    3066. Minimum Operations to Exceed Threshold Value II - Started

---
+++ Leetcode/3066. Minimum Operations to Exceed Threshold Value II/main.cpp
@@ -0,0 +1,20 @@
+// Khayotbek Mamajonov
+// DSAI - 02
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count = 0;
+        return count;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {18,43,36,13,7};
+    cout << s.maximumSum(nums) << endl;
+    return 0;
+}
