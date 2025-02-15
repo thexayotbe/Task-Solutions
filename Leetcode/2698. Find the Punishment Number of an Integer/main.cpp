89d56d8f4f7ae06696b5119ca5c8bcc66506e166
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-15 14:39:32 +0300

    2698. Find the Punishment Number of an Integer - Version one

--- Leetcode/2698. Find the Punishment Number of an Integer/main.cpp
+++ Leetcode/2698. Find the Punishment Number of an Integer/main.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello and welcome to "  << "!\n";
-    int name  = 22;
-    cout << name << endl;
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
-    }
+class Solution {
+public:
+    int punishmentNumber(int n) {
+        int punishmentNumber  = 0;
+        for (int i = 1; i <= n; i++) {
+            int squareNum =  i * i;
+            string stringNum = to_string(squareNum);
+            vector<vector<int>> memoArray(stringNum.size(),
+                                          vector<int>(i + 1, -1));
 
+            if (findPunishment(0, 0, stringNum, i, memoArray)) {
+                punishmentNumber += squareNum;
+            }
+        }
+        return punishmentNumber;
+    }
+    bool findPunishment(int startIndex,int sum, string stringNum,  int target, vector<vector<int>>& memo) {
+        if (startIndex == stringNum.length()) return sum == target;
+        if (sum > target) return false;
+        if (memo[startIndex][sum] != -1) return memo[startIndex][sum];
+
+        bool partionFound = false;
+
+        for (int currentIndex = startIndex; currentIndex < stringNum.length(); currentIndex++) {
+            string currentString = stringNum.substr(startIndex,  currentIndex - startIndex  + 1);
+
+            int added = stoi(currentString);
+
+            partionFound = partionFound || findPunishment(currentIndex + 1, sum + added, stringNum, target, memo);
+
+            if (partionFound == true) return true;
+        }
+        return memo[startIndex][sum] = partionFound;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n = 10;
+    cout << solution.punishmentNumber(n) << endl;
     return 0;
 }
