c5f82cfed5f155decba54cae0336019f2be2bf8d
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-15 13:00:46 +0300

    Started

---
+++ Leetcode/2698. Find the Punishment Number of an Integer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello and welcome to "  << "!\n";
+    int name  = 22;
+    cout << name << endl;
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
