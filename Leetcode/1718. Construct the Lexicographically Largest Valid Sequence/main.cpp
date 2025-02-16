744399d39a1fa189a319df79e5819d6b68d0d585
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-16 21:47:47 +0300

    1718. Construct the Lexicographically Largest Valid Sequence - Started

--- Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
+++ Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace  std;
 
+class Solution {
+public:
+    vector<int> constructDistancedSequence(int n) {
+
+    }
+};
+
 int main () {
+    Solution solution;
+    vector<int> v = solution.constructDistancedSequence(5);
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
     cout << "Hello World" << endl;
     return 0;
 };
