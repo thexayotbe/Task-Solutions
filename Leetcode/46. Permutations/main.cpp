5e4c1082672256637d53ee14ceb72d5ee4382905
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-17 15:25:57 +0300

    46. Permutations - Solved

--- Leetcode/46. Permutations/main.cpp
+++ Leetcode/46. Permutations/main.cpp
@@ -3,6 +3,21 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> results;
+
+        backtrack(nums, 0, results);
+        return results;
+    }
+    private:
+    void backtrack(vector<int> nums, int start, vector<vector<int>>& results) {
+        if ( start == nums.size() ) {
+            results.push_back(nums);
+        }
+        for ( int i = start; i < nums.size(); i++ ) {
+            swap( nums[i], nums[start] );
+            backtrack( nums, start + 1, results );
+            swap( nums[i], nums[start] );
+        }
 
     }
 };
