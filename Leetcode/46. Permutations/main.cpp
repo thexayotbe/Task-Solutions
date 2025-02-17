f103058a963a7cc6109c2a73cb38b984f034f127
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-17 15:26:50 +0300

    46. Permutations - Beats 100% in Runtime 0ms.

--- Leetcode/46. Permutations/main.cpp
+++ Leetcode/46. Permutations/main.cpp
@@ -1,4 +1,3 @@
-using namespace std;
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -7,17 +6,16 @@ public:
         backtrack(nums, 0, results);
         return results;
     }
-    private:
+
+private:
     void backtrack(vector<int> nums, int start, vector<vector<int>>& results) {
-        if ( start == nums.size() ) {
+        if (start == nums.size()) {
             results.push_back(nums);
         }
-        for ( int i = start; i < nums.size(); i++ ) {
-            swap( nums[i], nums[start] );
-            backtrack( nums, start + 1, results );
-            swap( nums[i], nums[start] );
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[i], nums[start]);
+            backtrack(nums, start + 1, results);
+            swap(nums[i], nums[start]);
         }
-
     }
 };
-
