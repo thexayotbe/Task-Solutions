4b43fceeec86d9b93352382aeb207727262b805a
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-17 17:46:22 +0300

    1718. Construct the Lexicographically Largest Valid Sequence - Solved

--- Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
+++ Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
@@ -1,41 +1,60 @@
 #include <iostream>
+#include <set>
 using namespace  std;
 
 class Solution {
 public:
     int numTilePossibilities(string tiless) {
-        int count = 0;
         vector<char> tiles(tiless.begin(), tiless.end());
+        sort(tiles.begin(), tiles.end());
         vector<vector<char>> result;
         vector<char> current;
         backtrack(tiles, 0, current, result);
-        for (int i = 0; i < result.size(); i++) {
-            for (int j = 0; j < result[i].size(); j++) {
-                cout << result[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << result.size();
+        int sum = 0;
 
-        return count;
+        for (int i = 0; i < result.size(); i++) {
+            set<vector<char>> permutaions;
+            permutation(result[i], 0, permutaions);
+            sum += permutaions.size();
+        }
+        return sum;
     }
 private:
     void backtrack(vector<char>& tiles, int start, vector<char> current, vector<vector<char>>& result) {
-        result.push_back(current);
-
+        if (!current.empty()) {
+            result.push_back(current);
+        }
         for (int i = start; i < tiles.size(); i++) {
+            if (i > start && tiles[i] == tiles[i - 1]) {
+                continue;
+            }
             current.push_back(tiles[i]);
             backtrack(tiles, i +1, current, result);
             current.pop_back();
         }
+    }
+    void permutation(vector<char>& tiles, int start, set<vector<char>>& result) {
+        if (start == tiles.size()) {
+            // Skip storing permutations that contain only spaces
+            if (any_of(tiles.begin(), tiles.end(), [](char c) { return c != ' '; })) {
+                result.insert(tiles);
+            }
+            return;
+        }
 
+        for (int i = start; i < tiles.size(); i++) {
+            if (tiles[i] == ' ') continue;
+            swap(tiles[start], tiles[i]);
+            permutation(tiles, start + 1, result);
+            swap(tiles[start], tiles[i]);
+        }
     }
 };
 
 
 int main () {
     Solution solution;
-    string const t = "V";
+    string const t = "AAABBC";
     cout << solution.numTilePossibilities(t) << endl << endl;
     return 0;
 };
