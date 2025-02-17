05dbadaa41f707238c805113b7c091cdea0d0bc6
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-17 15:04:31 +0300

    46. Permutations - Started

--- Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
+++ Leetcode/1718. Construct the Lexicographically Largest Valid Sequence/main.cpp
@@ -3,17 +3,39 @@ using namespace  std;
 
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
+    int numTilePossibilities(string tiless) {
         int count = 0;
+        vector<char> tiles(tiless.begin(), tiless.end());
+        vector<vector<char>> result;
+        vector<char> current;
+        backtrack(tiles, 0, current, result);
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << result.size();
 
         return count;
+    }
+private:
+    void backtrack(vector<char>& tiles, int start, vector<char> current, vector<vector<char>>& result) {
+        result.push_back(current);
+
+        for (int i = start; i < tiles.size(); i++) {
+            current.push_back(tiles[i]);
+            backtrack(tiles, i +1, current, result);
+            current.pop_back();
+        }
+
     }
 };
 
 
 int main () {
     Solution solution;
-    string const t = "AAB";
-    cout << solution.numTilePossibilities(t) << endl;
+    string const t = "V";
+    cout << solution.numTilePossibilities(t) << endl << endl;
     return 0;
 };
---
+++ Leetcode/46. Permutations/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1,2,3};
+    s.permute(nums);
+}
