d4c6084ca06b928c55411e09f34341bd89dbfaa4
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-21 10:48:09 +0300

    1261. Find Elements in a Contaminated Binary Tree - Started

---
+++ Leetcode/1261. Find Elements in a Contaminated Binary Tree /main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class FindElements {
+public:
+    unordered_set<int> values;
+
+    FindElements(TreeNode *root) {
+        recover(root, 0);
+    }
+
+    void recover(TreeNode *node, int val) {
+        if (!node) return;
+        node->val = val;
+        values.insert(val);
+
+        if (node->left != nullptr) recover(node->left, 2 * val + 1);
+        if (node->right != nullptr) recover(node->right, 2 * val + 2);
+    }
+
+    bool find(int target) {
+        return values.find(target) != values.end();
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
