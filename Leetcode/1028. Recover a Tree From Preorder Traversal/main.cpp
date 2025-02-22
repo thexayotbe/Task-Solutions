7f8752428d31f1ffba05df40406e7ae82d3ecae3
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-02-22 16:07:24 +0300

    1028. Recover a Tree From Preorder Traversal - Cleaned

--- Leetcode/1028. Recover a Tree From Preorder Traversal/main.cpp
+++ Leetcode/1028. Recover a Tree From Preorder Traversal/main.cpp
@@ -1,17 +1,18 @@
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
-};
+// This should not be submited
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {
+//     }
+//
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+//     }
+//
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+//     }
+// };
 
 class Solution {
 public:
