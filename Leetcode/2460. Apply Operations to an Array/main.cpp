dfa9655a8b6940c346fc8e451a9fa501ae9879bf
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-01 12:07:18 +0300

    2460. Apply Operations to an Array - Beats 100% in Runtime 0ms.

--- Leetcode/2460. Apply Operations to an Array/main.cpp
+++ Leetcode/2460. Apply Operations to an Array/main.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        for (int i = 0; i< nums.size()-1;i++) {
-            if (nums[i] == nums[i+1]) {
-                nums[i] = nums[i] *2;
-                nums[i+1] = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
             }
         }
         vector<int> ans;
         int zeros = 0;
-        for (int i = 0; i< nums.size();i++) {
-            if (nums[i] == 0) zeros++;
-            else ans.push_back(nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0)
+                zeros++;
+            else
+                ans.push_back(nums[i]);
         }
-        for (int i = 0; i< zeros;i++) {
+        for (int i = 0; i < zeros; i++) {
             ans.push_back(0);
         }
         return ans;
