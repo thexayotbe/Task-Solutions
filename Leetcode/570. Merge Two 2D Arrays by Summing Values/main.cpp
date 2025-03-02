48bc6347085ec3de6e4ecbf18f0d5f20e863ad24
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-02 10:07:39 +0300

    2570. Merge Two 2D Arrays by Summing Values - STARTED

---
+++ Leetcode/570. Merge Two 2D Arrays by Summing Values/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
