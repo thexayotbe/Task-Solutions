f1ce710f4f0254042c92f0cd6513e34db5727513
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-02 10:18:07 +0300

    2570. Merge Two 2D Arrays by Summing Values - Accepted

--- Leetcode/570. Merge Two 2D Arrays by Summing Values/main.cpp
+++ Leetcode/570. Merge Two 2D Arrays by Summing Values/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        unordered_map<int,int> m;
+        vector<vector<int>> res;
+        for (int i = 0; i < nums1.size(); i++) {
+            m[nums1[i][0]] = nums1[i][1];
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            m[nums2[i][0]]+=nums2[i][1];
+        }
+        for (auto it : m) {
+            vector<int> v;
+            int index = it.first;
+            int value = it.second;
+            v.push_back(index);
+            v.push_back(value);
+            res.push_back(v);
+        }
+        sort(res.begin(), res.end(), [](const vector<int>& v1, const vector<int>& v2) {
+            return v1[0] < v2[0];
+        });
 
+        return res;
     }
 };
 
 int main() {
+    vector<vector<int>> nums1 = {{1,2},{2,3},{4,5}}, nums2 = {{1,4},{3,2},{4,1}};
+    Solution solution;
+    solution.mergeArrays(nums1, nums2);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
