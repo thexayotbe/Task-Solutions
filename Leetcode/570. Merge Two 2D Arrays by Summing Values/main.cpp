8cfe336b1682ef7d1cf88ccc9e5019ead989a4ac
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-02 10:19:50 +0300

    2570. Merge Two 2D Arrays by Summing Values - Beats 100% in Runtime - 0ms.

--- Leetcode/570. Merge Two 2D Arrays by Summing Values/main.cpp
+++ Leetcode/570. Merge Two 2D Arrays by Summing Values/main.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        unordered_map<int,int> m;
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+                                    vector<vector<int>>& nums2) {
+        unordered_map<int, int> m;
         vector<vector<int>> res;
         for (int i = 0; i < nums1.size(); i++) {
             m[nums1[i][0]] = nums1[i][1];
         }
         for (int i = 0; i < nums2.size(); i++) {
-            m[nums2[i][0]]+=nums2[i][1];
+            m[nums2[i][0]] += nums2[i][1];
         }
         for (auto it : m) {
             vector<int> v;
@@ -17,10 +18,11 @@ public:
             v.push_back(value);
             res.push_back(v);
         }
-        sort(res.begin(), res.end(), [](const vector<int>& v1, const vector<int>& v2) {
-            return v1[0] < v2[0];
-        });
+        sort(res.begin(), res.end(),
+             [](const vector<int>& v1, const vector<int>& v2) {
+                 return v1[0] < v2[0];
+             });
+
         return res;
     }
 };
-
