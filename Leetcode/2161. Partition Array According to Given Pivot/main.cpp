438ee4ad24ab3f03722ace4afbf910baaa669ac0
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-03 20:37:11 +0300

    2161. Partition Array According to Given Pivot - Beats 100% in Runtime 0ms.

--- Leetcode/2161. Partition Array According to Given Pivot/main.cpp
+++ Leetcode/2161. Partition Array According to Given Pivot/main.cpp
@@ -1,27 +1,23 @@
-#include <iostream>
-using namespace std;
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> less;
         vector<int> great;
         int same = 0;
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             int num = nums[i];
-            if(num < pivot) less.push_back(num);
-            else if(num > pivot) great.push_back(num);
-            else same++;
+            if (num < pivot)
+                less.push_back(num);
+            else if (num > pivot)
+                great.push_back(num);
+            else
+                same++;
         }
-        while (same--) less.push_back(pivot);
-        for(int i : great) less.push_back(i);
+        while (same--)
+            less.push_back(pivot);
+        for (int i : great)
+            less.push_back(i);
 
-        return  less;
+        return less;
     }
 };
-int main() {
-    Solution s;
-    vector<int> nums = {9,12,5,10,14,3,10};
-    int pivot = 10;
-    s.pivotArray(nums, pivot);
-    return 0;
-}
