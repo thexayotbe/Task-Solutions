94bf55e08592ba3e3e128fa0d37315ab427d41d4
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-07 15:29:08 +0300

    2523. Closest Prime Numbers in Range - Beats 60% in Runtime 180ms.

--- Leetcode/2523. Closest Prime Numbers in Range/main.cpp
+++ Leetcode/2523. Closest Prime Numbers in Range/main.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
-        if (left > right) return {-1, -1};
+        if (left > right)
+            return {-1, -1};
 
         vector<bool> isPrime(right + 1, true);
         isPrime[0] = isPrime[1] = false;
 
-        for (int i = 2; i <= right; i++) {
+        for (int i = 2; i * i <= right; i++) {
             if (isPrime[i]) {
-                for (int j = i + i; j <= right; j += i) {
+                for (int j = i * i; j <= right; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
         vector<int> primes;
         for (int i = left; i <= right; i++) {
-            if (isPrime[i]) primes.push_back(i);
+            if (isPrime[i])
+                primes.push_back(i);
         }
 
-        if (primes.size() < 2) return {-1, -1};
+        if (primes.size() < 2)
+            return {-1, -1};
 
         int midDiff = INT_MAX, num1 = -1, num2 = -1;
 
@@ -36,10 +36,3 @@ public:
         return {num1, num2};
     }
 };
-
-int main() {
-    Solution s;
-    int left = 10, right = 19;
-    s.closestPrimes(left, right);
-    return 0;
-}
