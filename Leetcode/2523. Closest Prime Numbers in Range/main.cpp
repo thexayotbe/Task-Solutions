47a383b6ba2fa4f152dae097ada48ae4630fb3b4
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-07 13:34:30 +0300

    2523. Closest Prime Numbers in Range - Started

---
+++ Leetcode/2523. Closest Prime Numbers in Range/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> closestPrimes(int left, int right) {
+
+    }
+};
+
+int main() {
+    Solution s;
+    int  left = 10, right = 19;
+    s.closestPrimes(left, right);
+    return 0;
+}
