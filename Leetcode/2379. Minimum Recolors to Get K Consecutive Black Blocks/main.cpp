bbe069ae34245ef9ffd6eb909862b6163d7a2aaf
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-08 14:13:40 +0300

    2379. Minimum Recolors to Get K Consecutive Black Blocks - Accepted

--- Leetcode/2379. Minimum Recolors to Get K Consecutive Black Blocks/main.cpp
+++ Leetcode/2379. Minimum Recolors to Get K Consecutive Black Blocks/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        string window = "";
+        int whites = 0;;
+        for (int i = 0; i < k; i++) {
+            if (blocks[i] == 'W') whites++;
+            window+=blocks[i];
+        }
+        int minWhites = whites;
+        for (int i = k; i < blocks.size() ;i++) {
+            if (blocks[i] == 'W') {
+                whites++;
+            }
+            if (blocks[i-k] == 'W') whites--;
+
+            minWhites = min(minWhites, whites);
+        }
+        return minWhites;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution s;
+    string  blocks = "WBBWWBBWBW"; int k = 7;
+    cout << s.minimumRecolors(string(blocks), k);
     return 0;
 }
