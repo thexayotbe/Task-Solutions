c67300461839018976f1db7db7f7843b40a4a6b3
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-09 12:53:12 +0300

    3208. Alternating Groups II - Solved

--- Leetcode/3208. Alternating Groups II/main.cpp
+++ Leetcode/3208. Alternating Groups II/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int> &colors, int k) {
+        int n = colors.size();
+        int count = 0;
+        int left = 0;
+
+        for (int right = 0; right < n + k - 1; ++right) {
+            if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
+                left = right;
+            }
+
+            if (right - left + 1 >= k) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution s;
+    vector<int> colors = {0, 1, 0, 1, 0};
+    int k = 3;
+    cout << s.numberOfAlternatingGroups(colors, k) << endl;
     return 0;
 }
