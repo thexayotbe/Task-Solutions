f83fa8809f47774c504117b1778e5283128445d4
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-10 22:32:20 +0300

    3306. Count of Substrings Containing Every Vowel and K Consonants II - Accepted

--- Leetcode/3306. Count of Substrings Containing Every Vowel and K Consonants II/main.cpp
+++ Leetcode/3306. Count of Substrings Containing Every Vowel and K Consonants II/main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    long long countOfSubstrings(string word, int k) {
+        int frequencies[2][128] = {};
+        frequencies[0]['a'] = 1;
+        frequencies[0]['e'] = 1;
+        frequencies[0]['i'] = 1;
+        frequencies[0]['o'] = 1;
+        frequencies[0]['u'] = 1;
+
+        long long response = 0;
+        int currentK = 0;
+        int vowels = 0;
+        int extraLeft = 0;
+        for (int right = 0, left = 0; right < word.length(); right++) {
+            char rightChar = word[right];
+
+            if (frequencies[0][rightChar]) {
+                if (++frequencies[1][rightChar] == 1) vowels++;
+            } else {
+                currentK++;
+            }
+            while (currentK > k) {
+                char leftChar = word[left++];
+                if (frequencies[0][leftChar]) {
+                    if (--frequencies[1][leftChar] == 0) vowels--;
+                } else {
+                    currentK--;
+                }
+                extraLeft = 0;
+            }
+            while (vowels == 5 && currentK == k && left < right && frequencies[0][word[left]] && frequencies[1][word[
+                       left]] > 1) {
+                extraLeft++;
+                frequencies[1][word[left++]]--;
+            }
+
+            if (currentK == k && vowels == 5) {
+                response += (1 + extraLeft);
+            }
+        }
+
+
+        return response;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution s;
+    string word = "ieaouqqieaouqq";
+    int k = 1;
+    cout << s.countOfSubstrings(word, k) << endl;
     return 0;
 }
