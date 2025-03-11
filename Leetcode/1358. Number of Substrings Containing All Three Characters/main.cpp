84bf4ef8a6d228b03dee6f092d78b1685044f035
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-11 19:14:21 +0300

    1358. Number of Substrings Containing All Three Characters - Started

---
+++ Leetcode/1358. Number of Substrings Containing All Three Characters/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        unordered_map<char, int> m;
+    }
+};
+int main() {
+    string  s = "abcabc";
+    Solution solution;
+    cout << solution.numberOfSubstrings(s) << endl;
+    return 0;
+}
