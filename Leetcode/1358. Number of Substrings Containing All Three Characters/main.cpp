c9ca8a70a471db3af19785732d3bb910e42967ca
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-03-11 20:10:23 +0300

    1358. Number of Substrings Containing All Three Characters - Beats 100% in Runtime 0ms.

--- Leetcode/1358. Number of Substrings Containing All Three Characters/main.cpp
+++ Leetcode/1358. Number of Substrings Containing All Three Characters/main.cpp
@@ -5,9 +5,12 @@ public:
         int aIndex = -1, bIndex = -1, cIndex = -1;
 
         for (int i = 0; i < word.size(); i++) {
-            if (word[i] == 'a') aIndex = i;
-            if (word[i] == 'b') bIndex = i;
-            if (word[i] == 'c') cIndex = i;
+            if (word[i] == 'a')
+                aIndex = i;
+            if (word[i] == 'b')
+                bIndex = i;
+            if (word[i] == 'c')
+                cIndex = i;
 
             if (aIndex != -1 && bIndex != -1 && cIndex != -1) {
                 count += min({aIndex, bIndex, cIndex}) + 1;
@@ -16,6 +19,4 @@ public:
 
         return count;
     }
-
 };
-
